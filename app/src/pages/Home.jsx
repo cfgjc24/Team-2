9c7917a67b8b063698f6c1fd63eeb00348d8fd40
Kartik Vanjani <diyavanjani@Kartiks-Macbook-Air.local>
2024-09-20 19:27:26 -0400

    landing prog

---
+++ app/src/pages/Home.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div>
+      <div id="page-wrapper">
+
+
+    <section id="header" class="wrapper">
+
+
+            <div id="logo">
+                <h1><a href="index.html">First Generation Investors</a></h1>
+                <p>Join our course now!</p>
+            </div>
+
+
+            <nav id="nav">
+                <ul>
+                    <li class="current"><a href="index.html">Home</a></li>
+                    <li><a href="#">Modules</a></li>
+                </ul>
+            </nav>
+
+    </section>
+    <div/>
+    <div/>
+    <div/>
+
+
+
+    
+  )
+}
