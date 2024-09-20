0a097137c933298d7507423c9eb72eb7615a0421
Kayla <pandakay88@gmail.com>
2024-09-20 18:44:16 -0400

    Updates - Implemented skeleton for routing in Main.jsx

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -3,8 +3,29 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import {
+  createBrowserRouter,
+  RouterProvider
+} from "react-router-dom";
+
+// import the names of different page files
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/StudentPortal",
+    element: <Routes />,
+    errorElement: <ErrorPage />
+  }
+]);
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router = {router} />
   </StrictMode>,
 )
