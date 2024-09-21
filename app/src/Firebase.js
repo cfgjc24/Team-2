5cb8e4028b69219772d0220fe6197d561caf957b
Kayla <pandakay88@gmail.com>
2024-09-21 01:50:25 -0400

    Updates

--- app/src/Firebase.js
+++ app/src/Firebase.js
@@ -14,7 +14,6 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-const auth = getAuth(app);
 const firestore = getFirestore(app);
 const storage = getStorage(app);
 
--- app/src/components/module/module.jsx
+++ app/src/components/module/module.jsx
@@ -1,40 +1,27 @@
 import React from 'react'
 import { useState } from 'react'
 import './module.css'
-import StudentForm from '../Student_forms'
+import StudentForm from '../Pre_student_forms'
 
-
-function Module({ name, url }) {
-  function sendModule(name) {
-    return (
-      <>
-        <StudentForm
-          module={name}
-        />
-      </>
-    )
-
+function Module({name, url}) {
+  function sendModule(name){
+    StudentForm(name);
   }
   return (
     <>
-      <div className="module">
-        <h1 id="Module">Module {name}</h1>
-        <h5 id="description">
-          <div className="module">
-            <h1 id="Module">Module {name}</h1>
-            <h5 id="description">
-              Insert Description For Module
-            </h5>
-            <div className="body">
-              <ul>
-                <li onClick={() => sendModule(name)}><h3>Student Pre-Survey</h3></li>
-                <li><h3><a href={url}>Lesson Slides</a></h3></li>
-                <li><h3>Student Post-Survey</h3></li>
-              </ul>
-            </div>
-          </div>
-        </>
-        )
+      <div className = "module">
+        <h1 id = "Module">Module {name}</h1>
+        <h5 id = "description">
+          Insert Description For Module
+        </h5>
+          <ul>
+            <li><h3>Student Pre-Survey</h3></li>
+            <li><h3><a href = {url} onClick = {sendModule(name)}>Lesson Slides</a></h3></li>
+            <li><h3>Student Post-Survey</h3></li>
+          </ul>
+      </div>
+    </>
+  )
 }
 
-        export default Module
+export default Module
