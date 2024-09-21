5de01ed382bff5e60708beac63cb000bd48f003a
Kayla <pandakay88@gmail.com>
2024-09-21 00:10:30 -0400

    test

--- app/src/components/module/module.jsx
+++ app/src/components/module/module.jsx
@@ -1,24 +1,28 @@
 import React from 'react'
 import { useState } from 'react'
 import './module.css'
-import StudentForm from '../Pre_student_forms'
+import StudentForm from '../Student_forms'
 
-function Module({name, url}) {
-  function sendModule(name){
+function Module({ name, url }) {
+  function sendModule(name) {
+    StudentForm(name);
+  }
+  function sendModule(name) {
     StudentForm(name);
   }
   return (
     <>
-      <div className = "module">
-        <h1 id = "Module">Module {name}</h1>
-        <h5 id = "description">
+      <div className="module">
+        <h1 id="Module">Module {name}</h1>
+        <h5 id="description">
           Insert Description For Module
         </h5>
-          <ul>
-            <li><h3>Student Pre-Survey</h3></li>
-            <li><h3><a href = {url} onClick = {sendModule(name)}>Lesson Slides</a></h3></li>
-            <li><h3>Student Post-Survey</h3></li>
-          </ul>
+        <ul>
+          <li><h3>Student Pre-Survey</h3></li>
+          <li><h3><a href={url} onClick={sendModule(name)}>Lesson Slides</a></h3></li>
+          <li><h3><a href={url} onClick={sendModule(name)}>Lesson Slides</a></h3></li>
+          <li><h3>Student Post-Survey</h3></li>
+        </ul>
       </div>
     </>
   )
