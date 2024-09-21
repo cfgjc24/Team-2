9a11bc52dd47ce57f21d50af40fb1ccde8d86a16
sz389 <sz389@cornell.edu>
2024-09-20 23:24:23 -0400

    added Landing Page to the routes

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -3,15 +3,14 @@ import { useState } from "react";
 import "./App.css";
 import { ReactDOM } from "react";
 
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Admin from "../src/pages/Admin";
-//import Login from '../src/pages/Login'
-import Home from "../src/pages/Home";
-import LandingPage from "../src/pages/Landing_Page";
-import Login from "./pages/Login";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Admin from '../src/pages/Admin'
+import Login from '../src/pages/Login'
+import Home from '../src/pages/Home'
+import LandingPage from '../src/pages/LandingPage'
 
 export default function App() {
   return (
@@ -21,6 +20,7 @@ export default function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login/>}/>
         <Route path="/admin" element={<Admin />} />
+        <Route path="/landing" element={<LandingPage />} />
         <Route path="/landing" element={<LandingPage />} />
       </Routes>
     </Router>
