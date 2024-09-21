371abf6f9eed64a719ad3ae5adec8edf439cf182
Kartik Vanjani <diyavanjani@Kartiks-Macbook-Air.local>
2024-09-21 02:40:28 -0400

    r

--- app/src/pages/Login.jsx
+++ app/src/pages/Login.jsx
@@ -1,37 +1,54 @@
-import React from 'react'
-import {TextField, FormLabel} from '@mui/material';
-import { useState, useEffect } from 'react'
-import { ReactDOM } from 'react'
-import '../index.css'
-
+import React, { useState } from 'react';
+import { FormLabel, TextField, Button } from '@mui/material';
+import '../index.css';
 
 export default function Login() {
-    const [username, setUsername] = useState('')
-    const [password, setPassword] = useState('')
-    const [role, setRole] = useState('')
-    const [school, setSchool] = useState('')
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [role, setRole] = useState('');
+  const [school, setSchool] = useState('');
 
   return (
-    <div class = "login">
-      <form>
-  <div class = "form-group">
-    <FormLabel class = "label">Username: </FormLabel>
-    <TextField />
-  </div>
-  <div class = "form-group">
-    <FormLabel class = "label">Password: </FormLabel>
-    <TextField />
-  </div>
-  <div class = "form-group">
-    <FormLabel class = "label">Role: </FormLabel>
-    <TextField />
-  </div>
-  <div class = "form-group">
-    <FormLabel class = "label">School: </FormLabel>
-    <TextField />
-  </div>
-</form>
-<button type = "submit">Submit</button>
+    <div>
+      <h1 class = "login-header"><strong>Sign In To FGI</strong></h1>
+    <div className="login-container">
+     
+      <form className="login-form">
+      
+        <div className="form-group">
+          <FormLabel className="label">Username:</FormLabel>
+          <TextField
+            fullWidth
+            variant="outlined"
+          />
+        </div>
+        <div className="form-group">
+          <FormLabel className="label">Password:</FormLabel>
+          <TextField
+            fullWidth
+            variant="outlined"
+          />
+        </div>
+        <div className="form-group">
+          <FormLabel className="label">Role:</FormLabel>
+          <TextField
+            fullWidth
+            variant="outlined"
+          />
+        </div>
+        <div className="form-group">
+          <FormLabel className="label">School:</FormLabel>
+          <TextField
+            
+            fullWidth
+            variant="outlined"
+          />
+        </div>
+        <Button type="submit" variant="contained" color="primary" className="submit-button">
+          Submit
+        </Button>
+      </form>
     </div>
-  )
+    </div>
+  );
 }
