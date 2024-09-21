32972e80afdcd1a8dbafea959211a5c4d4f5af06
sz389 <sz389@cornell.edu>
2024-09-21 05:47:40 -0400

    edits to modules to include description

--- app/src/components/module/module.jsx
+++ app/src/components/module/module.jsx
@@ -3,20 +3,21 @@ import { useState } from 'react'
 import './module.css'
 import StudentForm from '../PreStudentForms'
 
-function Module({ name, url }) {
+function Module({ name, url, description }) {  // Receive description prop
   function sendModule(name) {
     StudentForm(name);
   }
+
   return (
     <>
       <div className="module">
         <h1 id="Module">Module {name}</h1>
         <h5 id="description">
-          Insert Description For Module
+          {description}  {/* Display the description */}
         </h5>
         <ul>
           <li><h3>Student Pre-Survey</h3></li>
-          <li><h3><a href={url} onClick={sendModule(name)}>Lesson Slides</a></h3></li>
+          <li><h3><a href={url} onClick={() => sendModule(name)}>Lesson Slides</a></h3></li>
           <li><h3>Student Post-Survey</h3></li>
         </ul>
       </div>
@@ -24,4 +25,4 @@ function Module({ name, url }) {
   )
 }
 
-export default Module
+export default Module;
