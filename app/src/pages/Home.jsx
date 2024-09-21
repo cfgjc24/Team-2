e99d78a62bb09366f956c489b77289e53b0901f6
Kartik Vanjani <diyavanjani@Kartiks-Macbook-Air.local>
2024-09-21 00:01:50 -0400

    updated home

--- app/src/pages/Home.jsx
+++ app/src/pages/Home.jsx
@@ -1,16 +1,278 @@
 import React from 'react'
 import '../index.css'
 
+
 export default function Home() {
   return (
     <div>
-        <h1>Home</h1>
-        <ul>
-  <li><a href="http://127.0.0.1:5173/">Home</a></li>
-  <li><a href="http://127.0.0.1:5173/login">Login</a></li>
-  <li><a href="http://127.0.0.1:5173/admin">Admin</a></li>
 
-</ul>
+      <div id="page-wrapper">
+
+
+
+        <header id="header" class="alt">
+          <h1 id="logo"><a href="index.html">First Generation Investors </a></h1>
+          <nav id="nav">
+            <ul>
+              <li class="current"><a href="http://127.0.0.1:5173/">Home</a></li>
+              <li class="submenu">
+                <a href="http://127.0.0.1:5173/admin">Admin</a>
+                <ul>
+                  <li><a href="left-sidebar.html">Left Sidebar</a></li>
+                  <li><a href="right-sidebar.html">Right Sidebar</a></li>
+                  <li><a href="no-sidebar.html">No Sidebar</a></li>
+                  <li><a href="contact.html">Contact</a></li>
+                  <li class="submenu">
+                    <a href="#">Submenu</a>
+                    <ul>
+                      <li><a href="#">Dolore Sed</a></li>
+                      <li><a href="#">Consequat</a></li>
+                      <li><a href="#">Lorem Magna</a></li>
+                      <li><a href="#">Sed Magna</a></li>
+                      <li><a href="#">Ipsum Nisl</a></li>
+                    </ul>
+                  </li>
+                </ul>
+              </li>
+              <li><a href="http://127.0.0.1:5173/login" class="button primary">Login</a></li>
+            </ul>
+          </nav>
+        </header>
+
+
+
+
+        <section id="banner">
+
+
+
+
+          <div class="inner">
+
+
+            <header>
+              <h2>First Generation Investors</h2>
+            </header>
+            <h3>Mission:</h3>
+            <p>First Generation Investors (FGI) teaches high school students the power of investing and brings
+classroom lessons to life by providing students with real money to invest.</p>
+            <footer>
+              <ul class="buttons stacked">
+                <li><a href="#main" class="button fit scrolly">Tell Me More</a></li>
+              </ul>
+            </footer>
+
+
+          </div>
+
+
+        </section>
+
+
+
+
+        <article id="main">
+
+
+          <header class="special container">
+            <span class="icon solid fa-chart-bar"></span>
+            <h2>Who are we?</h2>
+            <p>FGI believes that financial education is key to unlocking a brighter future. Many high school students
+may not have access to the resources they need to learn about investing and FGI is doing the work to
+bridge the gap.</p>
+          </header>
+
+
+
+          <section class="wrapper style2 container special-alt">
+            <div class="row gtr-50">
+              <div class="col-8 col-12-narrower">
+
+
+                <header>
+                  <h2>Behold the <strong>icons</strong> that visualize what you’re all about. or just take up space. your call bro.</h2>
+                </header>
+                <p>Sed tristique purus vitae volutpat ultrices. Aliquam eu elit eget arcu comteger ut fermentum lorem. Lorem ipsum dolor sit amet. Sed tristique purus vitae volutpat ultrices. eu elit eget commodo. Sed tristique purus vitae volutpat ultrices. Aliquam eu elit eget arcu commodo.</p>
+                <footer>
+                  <ul class="buttons">
+                    <li><a href="#" class="button">Find Out More</a></li>
+                  </ul>
+                </footer>
+
+
+              </div>
+              <div class="col-4 col-12-narrower imp-narrower">
+
+
+                <ul class="featured-icons">
+                  <li><span class="icon fa-clock"><span class="label">Feature 1</span></span></li>
+                  <li><span class="icon solid fa-volume-up"><span class="label">Feature 2</span></span></li>
+                  <li><span class="icon solid fa-laptop"><span class="label">Feature 3</span></span></li>
+                  <li><span class="icon solid fa-inbox"><span class="label">Feature 4</span></span></li>
+                  <li><span class="icon solid fa-lock"><span class="label">Feature 5</span></span></li>
+                  <li><span class="icon solid fa-cog"><span class="label">Feature 6</span></span></li>
+                </ul>
+
+
+              </div>
+            </div>
+          </section>
+
+
+
+          <section class="wrapper style1 container special">
+            <div class="row">
+              <div class="col-4 col-12-narrower">
+
+
+                <section>
+                  <span class="icon solid featured fa-check"></span>
+                  <header>
+                    <h3>This is Something</h3>
+                  </header>
+                  <p>Sed tristique purus vitae volutpat ultrices. Aliquam eu elit eget arcu commodo suscipit dolor nec nibh. Proin a ullamcorper elit, et sagittis turpis. Integer ut fermentum.</p>
+                </section>
+
+
+              </div>
+              <div class="col-4 col-12-narrower">
+
+
+                <section>
+                  <span class="icon solid featured fa-check"></span>
+                  <header>
+                    <h3>Also Something</h3>
+                  </header>
+                  <p>Sed tristique purus vitae volutpat ultrices. Aliquam eu elit eget arcu commodo suscipit dolor nec nibh. Proin a ullamcorper elit, et sagittis turpis. Integer ut fermentum.</p>
+                </section>
+
+
+              </div>
+              <div class="col-4 col-12-narrower">
+
+
+                <section>
+                  <span class="icon solid featured fa-check"></span>
+                  <header>
+                    <h3>Probably Something</h3>
+                  </header>
+                  <p>Sed tristique purus vitae volutpat ultrices. Aliquam eu elit eget arcu commodo suscipit dolor nec nibh. Proin a ullamcorper elit, et sagittis turpis. Integer ut fermentum.</p>
+                </section>
+
+
+              </div>
+            </div>
+          </section>
+
+
+
+          <section class="wrapper style3 container special">
+
+
+            <header class="major">
+              <h2>Next look at this <strong>cool stuff</strong></h2>
+            </header>
+
+
+            <div class="row">
+              <div class="col-6 col-12-narrower">
+
+
+                <section>
+                  <a href="#" class="image featured"><img src="images/pic01.jpg" alt="" /></a>
+                  <header>
+                    <h3>A Really Fast Train</h3>
+                  </header>
+                  <p>Sed tristique purus vitae volutpat commodo suscipit amet sed nibh. Proin a ullamcorper sed blandit. Sed tristique purus vitae volutpat commodo suscipit ullamcorper sed blandit lorem ipsum dolore.</p>
+                </section>
+
+
+              </div>
+              <div class="col-6 col-12-narrower">
+
+
+                <section>
+                  <a href="#" class="image featured"><img src="images/pic02.jpg" alt="" /></a>
+                  <header>
+                    <h3>An Airport Terminal</h3>
+                  </header>
+                  <p>Sed tristique purus vitae volutpat commodo suscipit amet sed nibh. Proin a ullamcorper sed blandit. Sed tristique purus vitae volutpat commodo suscipit ullamcorper sed blandit lorem ipsum dolore.</p>
+                </section>
+
+
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-6 col-12-narrower">
+
+
+                <section>
+                  <a href="#" class="image featured"><img src="images/pic03.jpg" alt="" /></a>
+                  <header>
+                    <h3>Hyperspace Travel</h3>
+                  </header>
+                  <p>Sed tristique purus vitae volutpat commodo suscipit amet sed nibh. Proin a ullamcorper sed blandit. Sed tristique purus vitae volutpat commodo suscipit ullamcorper sed blandit lorem ipsum dolore.</p>
+                </section>
+
+
+              </div>
+              <div class="col-6 col-12-narrower">
+
+
+                <section>
+                  <a href="#" class="image featured"><img src="images/pic04.jpg" alt="" /></a>
+                  <header>
+                    <h3>And Another Train</h3>
+                  </header>
+                  <p>Sed tristique purus vitae volutpat commodo suscipit amet sed nibh. Proin a ullamcorper sed blandit. Sed tristique purus vitae volutpat commodo suscipit ullamcorper sed blandit lorem ipsum dolore.</p>
+                </section>
+
+
+              </div>
+            </div>
+
+
+            <footer class="major">
+              <ul class="buttons">
+                <li><a href="#" class="button">See More</a></li>
+              </ul>
+            </footer>
+
+
+          </section>
+
+
+        </article>
+
+
+
+
+        <section id="cta">
+
+
+          <header>
+            <h2>Ready to learn <strong>something</strong>?</h2>
+            <p>Fill out our enrollment verification form and get started today! Like our mission, please do donate!</p>
+          </header>
+          <footer>
+            <ul class="buttons">
+              <li><a href="#" class="button primary">Enrollment Verification Form</a></li>
+              <li><a href="https://www.firstgenerationinvestors.com/donate.html" class="button">Donate</a></li>
+            </ul>
+          </footer>
+
+
+        </section>
+
+
+
+        
+
+
+      </div>
     </div>
   )
 }
+
+
+
