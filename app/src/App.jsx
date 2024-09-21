907329aad1576762c5583a54231c8bb1da02d96e
NabilHasan09 <nabilhasan123456@gmail.com>
2024-09-20 23:05:18 -0400

    implement student page with student form

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,9 +1,9 @@
 
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { ReactDOM } from 'react'
+import Student from './pages/Student'
+
 
 const root = document.getElementById('root')
-root.render(<Home/>)
+root.render(<Student/>)
+
