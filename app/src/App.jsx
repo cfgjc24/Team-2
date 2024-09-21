ffb849b517ce2ff97b1fc568ace3fd7632c3e1e3
Tobi Abidoye <tobiabidoye14@gmail.com>
2024-09-21 04:03:24 -0400

    Finished Student Form, worked in collaboration with Rishi Shah

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -12,7 +12,8 @@ import Admin from "../src/pages/Admin";
 import Home from "../src/pages/Home";
 import LandingPage from "../src/pages/Landing_Page";
 import Login from "./pages/Login";
-
+import Tutor from "./pages/Tutor";
+import Tutor_Survey from "./pages/Tutor_Survey";
 export default function App() {
   return (
     <Router>
@@ -22,6 +23,8 @@ export default function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/admin" element={<Admin />} />
         <Route path="/landing" element={<LandingPage />} />
+        <Route path="/tutor" element={<Tutor />} />
+        <Route path="/tutor_survey" element={<Tutor_Survey />} />
       </Routes>
     </Router>
   );
--- app/src/components/TutorModule/Module.jsx
+++ app/src/components/TutorModule/Module.jsx
@@ -4,11 +4,10 @@ import './Module.css';
 function Module({ name, url }) {
   return (
     <div className="module-card">
-      <h1 id="Class">{name}</h1>
-      <h5 id="description"> {name}</h5>
+      <h1 id="Class">Class {name}</h1>
       <ul>
         
-        <li><h3><a href={url} target="_blank" rel="noopener noreferrer">Class X</a></h3></li>
+        <li><h3><a href={url} target="_blank" rel="noopener noreferrer">Start Class</a></h3></li>
 
       </ul>
     </div>
