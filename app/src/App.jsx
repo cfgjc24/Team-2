cf659e0d917f9fbc136d51b19ee4ed64d4087e6a
Tobi Abidoye <tobiabidoye14@gmail.com>
2024-09-21 04:14:59 -0400

    routing commit

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -12,7 +12,9 @@ import Admin from "../src/pages/Admin";
 import Home from "../src/pages/Home";
 import LandingPage from "../src/pages/Landing_Page";
 import Login from "./pages/Login";
-import Student from "./pages/Student"
+import Student from "./pages/Student";
+import Tutor from "./pages/Tutor";
+import Tutor_Survey from "./pages/Tutor_Survey";
 
 export default function App() {
   return (
@@ -24,6 +26,8 @@ export default function App() {
         <Route path="/admin" element={<Admin />} />
         <Route path="/landing" element={<LandingPage />} />
         <Route path="/student" element={<Student />} />
+        <Route path="/tutor" element={<Tutor />} />
+        <Route path="/tutor_survey" element={<Tutor_Survey />} />
       </Routes>
     </Router>
   );
