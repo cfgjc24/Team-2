f50598e96efd9f9f54d82d01e9c492caeb7b4dc3
mafruhamanal <mafruhamanal1@gmail.com>
2024-09-21 00:09:36 -0400

    feat: firebase auth and storage setup

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -3,14 +3,15 @@ import { useState } from "react";
 import "./App.css";
 import { ReactDOM } from "react";
 
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
-import Admin from '../src/pages/Admin'
-import Login from '../src/pages/Login'
-import Home from '../src/pages/Home'
-import LandingPage from '../src/pages/LandingPage'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Admin from "../src/pages/Admin";
+//import Login from '../src/pages/Login'
+import Home from "../src/pages/Home";
+import LandingPage from "../src/pages/Landing_Page";
+import Login from "./pages/Login";
 
 export default function App() {
   return (
