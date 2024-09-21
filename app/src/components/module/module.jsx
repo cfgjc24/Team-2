6eb30a6cdc8ad6e92a31fdfb29b63f26c008ad99
Kayla <pandakay88@gmail.com>
2024-09-21 01:33:46 -0400

    Updates - fixed UI for modules

--- app/src/components/module/module.jsx
+++ app/src/components/module/module.jsx
@@ -3,29 +3,38 @@ import { useState } from 'react'
 import './module.css'
 import StudentForm from '../Student_forms'
 
+
 function Module({ name, url }) {
   function sendModule(name) {
-    StudentForm(name);
-  }
-  function sendModule(name) {
-    StudentForm(name);
+    return (
+      <>
+        <StudentForm
+          module={name}
+        />
+      </>
+    )
+
   }
   return (
     <>
       <div className="module">
         <h1 id="Module">Module {name}</h1>
         <h5 id="description">
-          Insert Description For Module
-        </h5>
-        <ul>
-          <li><h3>Student Pre-Survey</h3></li>
-          <li><h3><a href={url} onClick={sendModule(name)}>Lesson Slides</a></h3></li>
-          <li><h3><a href={url} onClick={sendModule(name)}>Lesson Slides</a></h3></li>
-          <li><h3>Student Post-Survey</h3></li>
-        </ul>
-      </div>
-    </>
-  )
+          <div className="module">
+            <h1 id="Module">Module {name}</h1>
+            <h5 id="description">
+              Insert Description For Module
+            </h5>
+            <div className="body">
+              <ul>
+                <li onClick={() => sendModule(name)}><h3>Student Pre-Survey</h3></li>
+                <li><h3><a href={url}>Lesson Slides</a></h3></li>
+                <li><h3>Student Post-Survey</h3></li>
+              </ul>
+            </div>
+          </div>
+        </>
+        )
 }
 
-export default Module
+        export default Module
