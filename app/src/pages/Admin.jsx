65eda6d8cf6d003d4d5f12c76682b056236fd564
Nicole Qiu <nfq2@cornell.edu>
2024-09-20 23:50:09 -0400

    Added student/tutor feedback cards

--- app/src/pages/Admin.jsx
+++ app/src/pages/Admin.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import { useState } from 'react'
 import PreConfidenceChart from '../components/PreConfidenceChart'
 import PostConfidenceChart from '../components/PostConfidenceChart'
+import StudentFeedback from '../components/StudentFeedback'
+import TutorFeedback from '../components/TutorFeedback'
 
-// import PostConfidenceChart from '../charts/Post-ConfidenceChart'
 
 function Admin() {
   return (
@@ -12,6 +12,11 @@ function Admin() {
       <PreConfidenceChart />
       <h1>Post-Confidence Survey</h1>
       <PostConfidenceChart />
+      <h1>View Feedback</h1>
+      <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+        <StudentFeedback />
+        <TutorFeedback />
+      </div>
     </div>
   )
 }
