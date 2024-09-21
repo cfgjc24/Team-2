f14c76e1b32db6b68708e3a4f1c53117a3ac3a28
NabilHasan09 <nabilhasan123456@gmail.com>
2024-09-20 23:06:14 -0400

    student form component

---
+++ app/src/pages/Student.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import StudentForm from '../components/student_forms'
+
+
+export default function Student() {
+ return (
+   // <student_forms/>
+       <StudentForm/>
+ )
+}
