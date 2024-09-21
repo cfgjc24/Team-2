11eec201a96c1edfcad6a372ee11ee5487ae904c
Kartik Vanjani <diyavanjani@Kartiks-Macbook-Air.local>
2024-09-20 21:01:16 -0400

    form

--- app/src/pages/Login.jsx
+++ app/src/pages/Login.jsx
@@ -2,19 +2,32 @@ import React from 'react'
 import {TextField, FormLabel} from '@mui/material';
 import { useState, useEffect } from 'react'
 import { ReactDOM } from 'react'
+import '../index.css'
 
 
 export default function Login() {
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    const [role, setRole] = useState('')
+    const [school, setSchool] = useState('')
 
   return (
     <div>
-      <form>
-  <div>
+      <form class = "form">
+  <div class = "username">
     <FormLabel>Username: </FormLabel>
     <TextField />
   </div>
-  <div>
+  <div class = "password">
     <FormLabel>Password: </FormLabel>
+    <TextField />
+  </div>
+  <div class = "password">
+    <FormLabel>Role: </FormLabel>
+    <TextField />
+  </div>
+  <div class = "password">
+    <FormLabel>School: </FormLabel>
     <TextField />
   </div>
 </form>
