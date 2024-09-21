d387b8e547c2b6dc8feaa9070d65c5cd8d185221
Kartik Vanjani <diyavanjani@Kartiks-Macbook-Air.local>
2024-09-20 20:23:19 -0400

    states

--- app/src/pages/Admin.jsx
+++ app/src/pages/Admin.jsx
@@ -2,8 +2,18 @@ import React from 'react'
 import { useState } from 'react'
 function Admin() {
   const [count, setCount] = useState(0)
+  const [module1, setModule1] = useState(0)
+  const [module2, setModule2] = useState(0)
+  const [module3, setModule3] = useState(0)
+  const [module4, setModule4] = useState(0)
+  const [module5, setModule5] = useState(0)
+  const [module6, setModule6] = useState(0)
+  const [module7, setModule7] = useState(0)
+  const [module8, setModule8] = useState(0)
+  const [loading, setLoading] = useState(false);
 
   return (
+
     <>
       <div>
       </div>
